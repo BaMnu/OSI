b59256a586c1fc303aa9c51f198f16364dc7c8c5
Olga Fedorova <bamnu13@gmail.com>
2022-10-06 18:14:49 +0300

    Second task

--- src/main/java/ru/netology/ClientThree.java
+++ src/main/java/ru/netology/ClientThree.java
@@ -1,10 +1,12 @@
 package ru.netology;
 
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Client {
+public class ClientThree {
     private static final int PORT = 8989;
     private static final String HOST = "127.0.0.1";
 
@@ -13,7 +15,7 @@ public class Client {
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
             System.out.println(in.readLine());
-            out.println("leeds");
+            out.println("sheffield");
             System.out.println(in.readLine());
 
         } catch (IOException e) {
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,42 +1,57 @@
 package ru.netology;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+    static String massage = "";
+    private static final int PORT = 8989;
 
-    private static final int PORT = 9090;
     public static void main(String[] args) throws IOException {
 
-        try (ServerSocket server = new ServerSocket(PORT); ) {
+        try (ServerSocket server = new ServerSocket(PORT)) {
             System.out.println("Server started");
 
             while (true) {
-                try (Socket clientSocket = server.accept();
-                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
-                {
-                    System.out.println("Client connection accepted");
 
-                    final String name = in.readLine();
-                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                try (Socket clientSocket = server.accept();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                ) {
+
+                    if (massage.equals("")) {
+                        out.println("???");
+
+                        massage = in.readLine();
+                        System.out.println(massage);
+
+                        out.println("OK");
+
+                    } else {
+                        out.println("Welcome. Previous city name: " + massage);
+
+                        String nextCity = in.readLine();
+
+                        char firstChar = nextCity.charAt(0);
+                        char lastChar = massage.charAt(massage.length() - 1);
+
+                        if (firstChar == lastChar) {
+                            out.println("OK");
+                            massage = nextCity;
+                        } else {
+                            out.println("NOT OK");
+                        }
+                    }
                 }
             }
+
         } catch (IOException io) {
-            throw new IOException(io);
+            System.out.println("Error. Can't start server.");
+            io.printStackTrace();
         }
     }
-
-    public static ServerSocket create(int[] ports) throws IOException {
-        for (int port : ports) {
-            try {
-                return new ServerSocket(port);
-            } catch (IOException ex) {
-                continue;
-            }
-        }
-        throw new IOException("No free port found");
-    }
-
 }
